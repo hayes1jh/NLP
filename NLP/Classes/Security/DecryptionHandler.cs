aadd6198865b52562b817cf4d2a6d3c2c78efa0f
hayes1jh <30508916+hayes1jh@users.noreply.github.com>
2024-12-20 13:15:02 -0800

    Add files via upload

---
+++ NLP/Classes/Security/DecryptionHandler.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLP
+{
+    public class DecryptionHandler
+    {
+        public static bool Decryption(string sInput, out string sOutput)
+        {
+            byte[] rgbKey = new byte[8] { 123, 56, 55, 23, 34, 98, 65, 69 };
+            byte[] rgbIV = new byte[8] { 1, 235, 55, 23, 34, 98, 65, 69 };
+
+            bool bResult = false;
+            try
+            {
+                SymmetricAlgorithm objAlgorithm = SymmetricAlgorithm.Create("DES");
+                ICryptoTransform objDecryptor = objAlgorithm.CreateDecryptor(rgbKey, rgbIV);
+
+                byte[] bInput = Convert.FromBase64String(sInput);
+
+
+                MemoryStream objOutput = new MemoryStream();
+                using (CryptoStream objCryptoStream = new CryptoStream(objOutput, objDecryptor, CryptoStreamMode.Write))
+                {
+                    objCryptoStream.Write(bInput, 0, bInput.Length);
+                    objCryptoStream.Flush();
+                    objCryptoStream.Close();
+                }
+
+                sOutput = Encoding.UTF8.GetString(objOutput.ToArray());
+                bResult = true;
+
+            }
+            catch (CryptographicException objError)
+            {
+                sOutput = objError.Message;
+            }
+
+            return bResult;
+        }
+
+
+        public static bool DecryptionWithExpiration(string sInput, out string sOutput)
+        {
+            bool bResult = false;
+            try
+            {
+                string sTemp;
+                if (Decryption(sInput, out sTemp))
+                {
+                    string[] _value = sTemp.Split('@');
+                    DateTime _date = DateTime.Parse(_value[1]);
+
+                    if (_date < DateTime.Now)
+                    {
+                        sOutput = "key is expired.";
+                    }
+                    else
+                    {
+                        bResult = true;
+                        sOutput = _value[0];
+                    }
+                }
+                else
+                {
+                    sOutput = sTemp;
+                }
+            }
+            catch (System.Exception objError)
+            {
+                sOutput = objError.Message;
+            }
+
+            return bResult;
+        }
+    }
+}
---
+++ NLP/Classes/Security/EncryptionHandler.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLP
+{ 
+    public class EncryptionHandler
+    {
+        public static bool Encryption(string sInput, out string sOutput)
+        {
+            byte[] rgbKey = new byte[8] { 123, 56, 55, 23, 34, 98, 65, 69 };
+            byte[] rgbIV = new byte[8] { 1, 235, 55, 23, 34, 98, 65, 69 };
+
+            bool bResult = false;
+            try
+            {
+                SymmetricAlgorithm objAlgorithm = SymmetricAlgorithm.Create("DES");
+                ICryptoTransform objEncryptor = objAlgorithm.CreateEncryptor(rgbKey, rgbIV);
+
+                byte[] bInput = Encoding.UTF8.GetBytes(sInput);
+
+                MemoryStream objOutput = new MemoryStream();
+                using (CryptoStream objCryptoStream = new CryptoStream(objOutput, objEncryptor, CryptoStreamMode.Write))
+                {
+                    objCryptoStream.Write(bInput, 0, bInput.Length);
+                    objCryptoStream.Flush();
+                    objCryptoStream.Close();
+                }
+
+                sOutput = Convert.ToBase64String(objOutput.ToArray());
+                bResult = true;
+            }
+            catch (CryptographicException objError)
+            {
+                sOutput = objError.Message;
+            }
+
+            return bResult;
+        }
+
+        public static bool EncryptionWithExpiration(string sInput, int iInput, out string sOutput)
+        {
+            bool bResult = false;
+
+            try
+            {
+                bResult = Encryption(sInput + "@" + DateTime.Now.AddDays(iInput).ToShortDateString(), out sOutput);
+            }
+            catch (Exception objError)
+            {
+                sOutput = objError.Message;
+            }
+
+            return bResult;
+        }
+    }
+}
